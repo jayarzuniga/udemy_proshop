0cff510d0ea7ccfd6bea0e20fa9020305168cf1b
Jay-r <jhayar.zuniga004@gmail.com>
2024-03-11 21:06:33 +0800

    section 2 done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,24 @@ import {Container}  from 'react-bootstrap'
 import Footer from './components/Footer';
 import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} exact />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
